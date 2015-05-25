a437b46fd7722bad2777f45632c3c4cd68837ca7
Kenneth LeFebvre <kenneth@lefebvre.us>
2015-05-25 15:17:30 -0400

    Runs on Android. Builds on iOS and Windows Phone.

---
+++ src/DiabetesLog/DiabetesLog/DiabetesLog/HomePage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace DiabetesLog
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+            Title = "Cleveland .NET SIG - Diabetes Log";
+        }
+    }
+}
