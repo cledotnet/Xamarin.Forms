f27f571ae7655e7d787b516843bb8919bec05402
Kenneth LeFebvre <kenneth@lefebvre.us>
2015-05-25 14:12:43 -0400

    Create new Xamarin.Forms app from template

---
+++ src/DiabetesLog/DiabetesLog/DiabetesLog.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using DiabetesLog.WinPhone.Resources;
+
+namespace DiabetesLog.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
